0233340cb54fbe05517a109d85df020d09e7376f
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-07-11 12:24:18 -0300

    Add queries for questions given

--- queries.sql
+++ queries.sql
@@ -66,3 +66,22 @@ WHERE weight_kg < 0;
 
 /*Commit transaction*/
 COMMIT;
+
+/*------- questions ---------*/
+/*How many animals are there?*/
+SELECT COUNT(*) FROM animals;
+
+/*How many animals have never tried to escape?*/
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/*What is the average weight of animals?*/
+SELECT AVG(weight_kg) FROM animals;
+
+/*Who escapes the most, neutered or not neutered animals?*/
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
+
+/*What is the minimum and maximum weight of each type of animal?*/
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+/*What is the average number of escape attempts per animal type of those born between 1990 and 2000?*/
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-1-1' AND '1999-12-31' GROUP BY species;
